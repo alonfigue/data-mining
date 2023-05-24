a79fa99969a7dc52b02bf622c8139511db6f78c5
alonfigue <alonsogfm2000@gmail.com>
2023-05-24 14:29:55 -0400

    filling tables

--- tables.sql
+++ tables.sql
@@ -7,15 +7,15 @@ CREATE DATABASE Data_Mining;
 -- Creación de tablas
 CREATE TABLE Clientes(
     id_cliente SERIAL PRIMARY KEY,
-    nombre_cliente varchar(30) NOT NULL,
-    apellido varchar(30) NOT NULL,
-    correo varchar(30) NOT NULL,
-    domicilio varchar(30)
+    nombre_cliente varchar(255) NOT NULL,
+    apellido varchar(255) NOT NULL,
+    correo varchar(255) NOT NULL,
+    domicilio varchar(255)
 );
 
 CREATE TABLE Productos(
     id_producto SERIAL PRIMARY KEY,
-    nombre_producto varchar(30) NOT NULL,
+    nombre_producto varchar(255) NOT NULL,
     costo varchar(30) NOT NULL,
     elaborado_por varchar(30),
     existencia boolean
@@ -28,14 +28,13 @@ CREATE TABLE Recibos(
     id_producto int NOT NULL,
     FOREIGN KEY (id_producto) REFERENCES Productos(id_producto),
     fecha date NOT NULL, --YYYY-MM-DD
-	hora varchar(10),
-    dia_de_la_semana varchar(9),
+	hora varchar(20),
+    dia_de_la_semana varchar(30),
     cantidad int NOT NULL,
-    modo_pago varchar(30) NOT NULL
+    modo_pago varchar(30)
 );
 
 -- llenado de tablas:
-
 -- Clientes:
 INSERT INTO Clientes(nombre_cliente, apellido, correo, domicilio) VALUES('Alonso', 'Figueroa', 'ag@gmail.com', 'Av. 5 de Julio con El Milagro');
 
@@ -44,3 +43,12 @@ INSERT INTO Productos(nombre_producto, costo, elaborado_por, existencia) VALUES(
 
 -- Recibos:
 INSERT INTO Recibos(id_cliente, id_producto, fecha, hora, dia_de_la_semana, cantidad, modo_pago) VALUES('1', '1', '2023-05-23', '10:41 p.m.', 'Martes', 4, 'Zelle');
+
+
+-- borrar filas
+delete from clientes where nombre_cliente='Alonso';
+delete from productos where nombre_producto='Cerveza';
+delete from recibos where dia_de_la_semana='Martes';
+
+--eliminar tabla
+drop table clientes;
